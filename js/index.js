09f4cef9f5f3cda036e7943186fe1f3dd5d4b08a
simrat06 <93942135+simrat06@users.noreply.github.com>
2022-05-31 06:19:41 +0530

    update
    update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 console.log("your index.js file is loaded correctly!!")
+
+$(document).ready(function(){
+    $("h2").hover(function(){
+      $(this).css("text-decoration", "underline");
+      }, function(){
+      $(this).css("text-decoration", "none");
+    });
+  });
+
+
+ 
